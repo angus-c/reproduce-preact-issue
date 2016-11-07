7e635ba7ec724e612ebe7a661508aa6db77c30bd
Angus Croll <angus@uber.com>
2016-11-07 14:32:26 -0800

    key clashing

--- src/components/parent/index.js
+++ src/components/parent/index.js
@@ -7,21 +7,21 @@ export default class Parent extends Component {
 		this.state = {
 			childComponentIndex: 1
 		};
-		
+
 		this.childComponents = [Comp1, Comp2];
 	}
 
 	render() {
-		const ChildComponent = this.childComponents[this.state.childComponentIndex];
+		const ChildComponent = this.childComponents[this.state.childComponentIndex - 1];
 		return (
 			<div>
-				<button onClick={() => this.switch(0)}>Switch to 1</button>
-				<button onClick={() => this.switch(1)}>Switch to 3</button>
-				<ChildComponent/>
+				<button onClick={() => this.switch(1)}>Switch to 1</button>
+				<button onClick={() => this.switch(2)}>Switch to 2</button>
+				<ChildComponent names={this.state.names} />
 			</div>
 		);
 	}
-	
+
 	switch(index) {
 		this.setState({childComponentIndex: index});
 	}
@@ -30,7 +30,7 @@ export default class Parent extends Component {
 class Comp1 extends Component {
   render() {
 		return (
-			<Comp3 label={'comp2'}/>
+      <div>{'Comp1'}</div>
     );
   }
 }
@@ -38,24 +38,34 @@ class Comp1 extends Component {
 class Comp2 extends Component {
   render() {
 		return (
-      <div>
-				<Comp3 label={'comp2'}/>
+			<div>
+				<Comp3>
+					<Comp4 key={1}>{'child'}</Comp4>
+				</Comp3>
 			</div>
     );
   }
 }
 
-
 class Comp3 extends Component {
   render() {
 		return (
 			<div>
-				<div>
-					<div>hello1</div>
-					<div>hello2</div>
-				</div>
-				<span>{this.props.label}</span>
+				{this.props.children}
+				<Comp4 key={1}>{'inline'}</Comp4>
 			</div>
     );
+  }
+}
+
+class Comp4 extends Component {
+  render() {
+		return (
+			<div>
+				{('' + Date.now()).substr(-3)}
+				{' '}
+				{this.props.children}
+			</div>
+		);
   }
 }
